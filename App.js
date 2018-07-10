850875c1c48c3ed62890612dbbd4d71e2927139c
Talon So <Talon_So@Hotmail.com>
2018-07-10 17:03:38 -0400

    added buttons for login page

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+
+    onLoginPressed(){
+    alert("Login Successful")
+    }
+    onRegisterPressed(){
+      alert("Registration Begin")
+    }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text style={styles.title}>Code Mangler</Text>
+        <View style={styles.btnContainer}>
+          <Button
+            onPress={this.onLoginPressed}
+            title="Login"/>
+            <Button
+              onPress={this.onRegisterPressed}
+              title="Register"/>
+            </View>
       </View>
     );
   }
@@ -19,5 +33,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    color: '#a4c3b2',
+    fontSize: 48,
+  },
+  btnContainer: {
+    margin: 20,
   },
 });
